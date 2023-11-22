e67e1c57ff1b1c997eff9848a88c1c37bc108e07
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-11-22 06:39:11 -0500

    Add basic structure for game

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
 'use strict';
 
 
+
+const game = (function () {
+
+    const handleBoard = (function () {
+        let board = [
+            'x','o','x',
+            'o','o','o',
+            'x','','x',
+        ];
+
+        return {board};
+    })();
+
+
+    const firstPlayer = (function () {
+        let name = 'player1';
+
+        return {name};
+    })();
+
+
+    const secondPlayer = (function () {
+        let name = 'player2';
+
+        return {name};
+    })();
+
+
+    return { handleBoard, firstPlayer, secondPlayer, checkWin };
+})();
+
+console.log(game);
+
+console.log(game.handleBoard);
+console.log(game.firstPlayer);
+console.log(game.secondPlayer);
+
+
+
+
+
+
+
+
