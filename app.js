a4595e006de04a47b940a50c08de9e2914761708
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-11-22 10:34:44 -0500

    Refactor checkWin module to be a single Function

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const game = (function () {
 
     const handleBoard = (function () {
         let board = [
-            ['0','1','2'],
-            ['3','4','5'],
-            ['6','7','8'],
+            ['x','x','x'],
+            ['o','o','o'],
+            ['x','x','x'],
         ];
 
         return {board};
@@ -29,145 +29,123 @@ const game = (function () {
     })();
 
 
-    const checkWin = (function () {
-        
+    function checkWin() {
         let x = 0;
         let o = 0;
 
-        function horizontalWinCheck() {
-            for (let i = 0; i < handleBoard.board.length; i++){
-
-                for (let a = 0; a < handleBoard.board[i].length; a++) {
-
-                    if (handleBoard.board[i][a] === 'x'){
-                        x++;
-                        o = 0;
-                    } else if (handleBoard.board[i][a] === 'o'){
-                        o++;
-                        x = 0;
-                    } else {
-                        x = 0;
-                        o = 0;
-                    };
-                };
-
-                if (x === 3){
-                    console.log('x won horizontally');
-                } else if (o === 3){
-                    console.log('o won horizontally');
+        //CHECKING HORIZONTAL WINS
+        for (let i = 0; i < handleBoard.board.length; i++){
+            for (let a = 0; a < handleBoard.board[i].length; a++) {
+                if (handleBoard.board[i][a] === 'x'){
+                    x++;
+                    o = 0;
+                } else if (handleBoard.board[i][a] === 'o'){
+                    o++;
+                    x = 0;
                 } else {
-                    console.log('nobody won horizontally');
+                    x = 0;
+                    o = 0;
                 };
             };
-            x = 0;
-            o = 0;
+
+            if (x === 3){
+                return 'x wins!';
+            } else if (o === 3){
+                return 'o wins!';
+            } else {
+                x = 0;
+                o = 0;
+            };
         };
 
-        
-        function verticalWinCheck() {
+        //CHECKING VERTICAL WINS
+        let col1 = [];
+        let col2 = [];
+        let col3 = [];
 
-            let col1 = [];
-            let col2 = [];
-            let col3 = [];
 
-            //createing vertical arrays
-            for (let i = 0; i < handleBoard.board.length; i++){
-                col1.push(handleBoard.board[i][0]);
-                col2.push(handleBoard.board[i][1]);
-                col3.push(handleBoard.board[i][2]);
-            };
-            let verticalArrays = [col1, col2, col3];
+        for (let i = 0; i < handleBoard.board.length; i++){//creating verticalArrays
+            col1.push(handleBoard.board[i][0]);
+            col2.push(handleBoard.board[i][1]);
+            col3.push(handleBoard.board[i][2]);
+        };
+        let verticalArrays = [col1, col2, col3];
 
-            //checking vertical arrays for matches
-            for (let i = 0; i < verticalArrays.length; i++){
-
-                for (let a = 0; a < verticalArrays[i].length; a++){
-
-                    if (verticalArrays[i][a] === 'x'){
-                        x++;
-                        o = 0;
-                    } else if (verticalArrays[i][a] === 'o'){
-                        o++;
-                        x = 0;
-                    } else {
-                        x = 0;
-                        o = 0;
-                    };
-                };
-
-                if (x === 3){
-                    console.log('x won vertically');
-                } else if (o === 3) {
-                    console.log('o won vertically');
+        for (let i = 0; i < verticalArrays.length; i++){//checking verticalArrays for matches
+            for (let a = 0; a < verticalArrays[i].length; a++){
+                if (verticalArrays[i][a] === 'x'){
+                    x++;
+                    o = 0;
+                } else if (verticalArrays[i][a] === 'o'){
+                    o++;
+                    x = 0;
                 } else {
-                    console.log('nobody won vertically');
+                    x = 0;
+                    o = 0;
                 };
             };
-            x = 0;
-            o = 0;
+
+            if (x === 3){
+                return 'x wins!';
+            } else if (o === 3) {
+                return 'o wins!';
+            } else {
+                x = 0;
+                o = 0;
+            };
         };
 
 
-        function diagonalWinCheck() {
+        //CHECKING DIAGONAL WINS
+        let diag1 = [
+            handleBoard.board[0][0],
+            handleBoard.board[1][1],
+            handleBoard.board[2][2]
+        ];
+        let diag2 = [
+            handleBoard.board[0][2],
+            handleBoard.board[1][1],
+            handleBoard.board[2][0]
+        ];
+        let diagonalArrays = [diag1, diag2];
 
-            let diag1 = [
-                handleBoard.board[0][0],
-                handleBoard.board[1][1],
-                handleBoard.board[2][2]
-            ];
 
-            let diag2 = [
-                handleBoard.board[0][2],
-                handleBoard.board[1][1],
-                handleBoard.board[2][0]
-            ];
-
-            let diagonalArrays = [diag1, diag2];
-
-            for (let i = 0; i < diagonalArrays.length; i++) {
-
-                for (let a = 0; a < diagonalArrays[i].length; a++) {
-
-                    if (diagonalArrays[i][a] === 'x'){
-                        x++;
-                        o = 0;
-                    } else if (diagonalArrays[i][a] === 'o'){
-                        o++;
-                        x = 0;
-                    } else {
-                        x = 0;
-                        o = 0;
-                    };
-                };
-
-                if (x === 3) {
-                    console.log('x won diagonally');
-                } else if (o === 3) {
-                    console.log('o won diagonally');
+        for (let i = 0; i < diagonalArrays.length; i++) {
+            for (let a = 0; a < diagonalArrays[i].length; a++) {
+                if (diagonalArrays[i][a] === 'x'){
+                    x++;
+                    o = 0;
+                } else if (diagonalArrays[i][a] === 'o'){
+                    o++;
+                    x = 0;
                 } else {
-                    console.log('nobody won diagonally');
+                    x = 0;
+                    o = 0;
                 };
             };
-            x = 0;
-            o = 0;
+
+            if (x === 3) {
+                return 'x wins!';
+            } else if (o === 3) {
+                return 'o wins!';
+            } else {
+                x = 0;
+                o = 0;
+            };
         };
-
-
-        return { horizontalWinCheck, verticalWinCheck, diagonalWinCheck };
-
-    })();
-
+    };
 
     return { handleBoard, firstPlayer, secondPlayer, checkWin };
 })();
 
-console.log(game);
+// console.log(game);
 
-console.log(game.handleBoard);
-console.log(game.firstPlayer);
-console.log(game.secondPlayer);
+// console.log(game.handleBoard);
+// console.log(game.firstPlayer);
+// console.log(game.secondPlayer);
 
-game.checkWin.horizontalWinCheck();
-game.checkWin.verticalWinCheck();
-game.checkWin.diagonalWinCheck();
+console.log(game.checkWin());
 
+if (game.checkWin() === 'x wins!'){
+    console.log('test');
+};
