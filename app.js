52c60fc6985c4bfcba19d167dc7dffe677a637bc
Dreary Dev <DrearyDev@proton.me>
2023-11-30 12:26:39 -0500

    Make game playable in console

--- app.js
+++ app.js
@@ -10,23 +10,24 @@ function gameController() {
         let board = [[],[],[]];
         const container = document.querySelector('.container');
 
-        for (let i = 0; i < 3; i++){
-            for (let a = 0; a < 3; a++){
-                console.log(a);
-                let div = document.createElement('div');
-                board[i][a] = { div };
-                container.appendChild(board[i][a].div);
+        (function newBoard() {
+            for (let i = 0; i < 3; i++){
+                for (let a = 0; a < 3; a++){
+                    let div = document.createElement('div');
+                    board[i][a] = { div };
+                    container.appendChild(board[i][a].div);
+                    board[i][a].symbol = '';
+                };
+            };
+        })();
+
+        function removeBoard() {
+            while (container.firstChild){
+                container.removeChild(container.lastChild);
             };
         };
 
-        // board.forEach(obj => {
-        //     obj.div.addEventListener('click', (e) => {
-        //         console.log('clicked');
-        //         console.log(e);
-        //     });
-        // });
-
-        return {board};
+        return {board, removeBoard};
     })();
 
     function createPlayer(name, symbol){
@@ -41,10 +42,10 @@ function gameController() {
         //CHECKING HORIZONTAL WINS
         for (let i = 0; i < board.length; i++){
             for (let a = 0; a < board[i].length; a++) {
-                if (board[i][a] === 'x'){
+                if (board[i][a].symbol === 'x'){
                     x++;
                     o = 0;
-                } else if (board[i][a] === 'o'){
+                } else if (board[i][a].symbol === 'o'){
                     o++;
                     x = 0;
                 } else {
@@ -67,9 +68,9 @@ function gameController() {
         let col3 = [];
 
         for (let i = 0; i < board.length; i++){//creating verticalArrays
-            col1.push(board[i][0]);
-            col2.push(board[i][1]);
-            col3.push(board[i][2]);
+            col1.push(board[i][0].symbol);
+            col2.push(board[i][1].symbol);
+            col3.push(board[i][2].symbol);
         };
         let verticalArrays = [col1, col2, col3];
 
@@ -97,14 +98,14 @@ function gameController() {
 
         //CHECKING DIAGONAL WINS
         let diag1 = [
-            board[0][0],
-            board[1][1],
-            board[2][2]
+            board[0][0].symbol,
+            board[1][1].symbol,
+            board[2][2].symbol
         ];
         let diag2 = [
-            board[0][2],
-            board[1][1],
-            board[2][0]
+            board[0][2].symbol,
+            board[1][1].symbol,
+            board[2][0].symbol
         ];
         let diagonalArrays = [diag1, diag2];
 
@@ -134,19 +135,15 @@ function gameController() {
     };
 
     const updateBoard = function(row, col, symbol) {
-        if (handleBoard.board[row][col] !== ''){
+        if (handleBoard.board[row][col].symbol !== ''){
             return 'spot not empty!';
         };
 
-        handleBoard.board[row][col] = symbol;
+        handleBoard.board[row][col].symbol = symbol;
     };
 
     function resetGame() {
-        handleBoard.board = [
-            ['','',''],
-            ['','',''],
-            ['','','']
-        ];
+        handleBoard.removeBoard();
 
         gameController();
     };
